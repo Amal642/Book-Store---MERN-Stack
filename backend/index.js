41417876c6096bf25342df622abbf4697cf937a5
amal642 <mohamedamal0345@gmail.com>
2024-02-25 17:27:52 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express, { response } from "express";
+import {PORT,mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import {Book} from "./models/bookModel.js";
+import booksRoute from './routes/bookRoutes.js';
+import cors from 'cors';
+
+const app=express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/',(request,response)=>{
+    console.log(request);
+    return response.status(234).send("Marhaban Ya akhi va ukhti,Sabah al khair");
+});
+
+app.use('/books',booksRoute);
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log("App Connected to DB");
+        app.listen(PORT,()=>{
+            console.log(`App is running in http://localhost:${PORT}`);
+        });
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
